3b79310521c0823b7a75204ffa1eac862ef8d17e
Jiju S Jacob <jiju@keyvalue.systems>
2023-06-20 14:34:27 +0530

    feat: add sdk

---
+++ sdk1000.xcframework/ios-arm64/sdk1000.framework/Headers/sdk1000.h
@@ -0,0 +1,18 @@
+//
+//  sdk1000.h
+//  sdk1000
+//
+//  Created by JIju S Jacob on 20/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sdk1000.
+FOUNDATION_EXPORT double sdk1000VersionNumber;
+
+//! Project version string for sdk1000.
+FOUNDATION_EXPORT const unsigned char sdk1000VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk1000/PublicHeader.h>
+
+
